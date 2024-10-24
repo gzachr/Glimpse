09fe82194092652777a0245c83c11e56b844b54c
Francis Isip <francis_bawa@dlsu.edu.ph>
2024-10-24 11:15:10 +0800

    minor changes

--- app/src/main/java/com/mobdeve/s12/group8/glimpse/GalleryActivity.kt
+++ app/src/main/java/com/mobdeve/s12/group8/glimpse/GalleryActivity.kt
@@ -5,20 +5,18 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.mobdeve.s12.group8.glimpse.model.Post
 
 class GalleryActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
-    private lateinit var postsList: PostsList
+    private var data: ArrayList<Post> = DataHelper.loadPostData()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery)
         recyclerView = findViewById(R.id.recyclerViewPosts)
 
-        postsList = PostsList()
-
         recyclerView.layoutManager = GridLayoutManager(this, 3)
-        Log.d("GalleryActivity", "Post list size: ${postsList.postList.size}")
-        recyclerView.adapter = GalleryAdapter(postsList.postList)
+        recyclerView.adapter = GalleryAdapter(data)
     }
 }
--- app/src/main/java/com/mobdeve/s12/group8/glimpse/GalleryAdapter.kt
+++ app/src/main/java/com/mobdeve/s12/group8/glimpse/GalleryAdapter.kt
@@ -1,12 +1,15 @@
 package com.mobdeve.s12.group8.glimpse
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity.RESULT_OK
 import androidx.recyclerview.widget.RecyclerView
+import com.mobdeve.s12.group8.glimpse.model.Post
 
-class GalleryAdapter(private val postList: List<Int>) : RecyclerView.Adapter<GalleryAdapter.PostViewHolder>() {
+class GalleryAdapter(private val postList: ArrayList<Post>) : RecyclerView.Adapter<GalleryAdapter.PostViewHolder>() {
     inner class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imageView: ImageView = itemView.findViewById(R.id.imageViewPost)
     }
@@ -22,7 +25,13 @@ class GalleryAdapter(private val postList: List<Int>) : RecyclerView.Adapter<Gal
 
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
         val imageResId = postList[position]
-        holder.imageView.setImageResource(imageResId)
+        holder.imageView.setImageResource(imageResId.postImageId)
+
+        holder.imageView.setOnClickListener {
+            val context = holder.imageView.context
+            val intent = Intent(context, FeedActivity::class.java)
+            context.startActivity(intent)
+        }
     }
 
 }
