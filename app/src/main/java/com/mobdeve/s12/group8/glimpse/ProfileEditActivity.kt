0ec9643933510ba6dcab9dc31a9e7cceaa037cea
ricardolu1s <113566864+ricardolu1s@users.noreply.github.com>
2024-11-05 15:01:15 +0800

    profiles

--- app/src/main/java/com/mobdeve/s12/group8/glimpse/ProfileEditActivity.kt
+++ app/src/main/java/com/mobdeve/s12/group8/glimpse/ProfileEditActivity.kt
@@ -1,8 +1,15 @@
 package com.mobdeve.s12.group8.glimpse
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.transition.Transition
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.mobdeve.s12.group8.glimpse.databinding.ActivityProfileEditBinding
 
 class ProfileEditActivity : AppCompatActivity() {
@@ -18,5 +25,16 @@ class ProfileEditActivity : AppCompatActivity() {
         binding.profileEditSaveButton.setOnClickListener{
             finish()
         }
+        Glide.with(this)
+            .load(R.drawable.user1)
+            .apply(RequestOptions().transform(RoundedCorners(1000)))
+            .into(binding.currentProfileImage)
+
+        Glide.with(this)
+            .load(R.drawable.upload_profile_photo)
+            .apply(RequestOptions()
+                .transform(CircleCrop()))
+            .into(binding.profileEditImage)
+
     }
 }
