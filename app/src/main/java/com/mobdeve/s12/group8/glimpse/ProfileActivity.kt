f9216bf6eead7b2c6e9d9c73a3011eaedd44f67c
ricardolu1s <113566864+ricardolu1s@users.noreply.github.com>
2024-11-05 11:55:36 +0800

    profile circle image and button layout

--- app/src/main/java/com/mobdeve/s12/group8/glimpse/ProfileActivity.kt
+++ app/src/main/java/com/mobdeve/s12/group8/glimpse/ProfileActivity.kt
@@ -1,8 +1,12 @@
 package com.mobdeve.s12.group8.glimpse
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.mobdeve.s12.group8.glimpse.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
@@ -19,5 +23,13 @@ class ProfileActivity : AppCompatActivity() {
             val intent = Intent(applicationContext, ProfileEditActivity::class.java)
             startActivity(intent)
         }
+        binding.profileEditLabel.setOnClickListener{
+            val intent = Intent(applicationContext, ProfileEditActivity::class.java)
+            startActivity(intent)
+        }
+        Glide.with(this)
+            .load(R.drawable.user1)
+            .apply(RequestOptions().transform(RoundedCorners(1000)))
+            .into(binding.profileImage)
     }
 }
